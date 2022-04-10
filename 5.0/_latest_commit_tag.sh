87da8f732ec936b0aacef73cc356a2dbb2491b02
JGWill Gaia.WSL <jgi@jgwill.com>
2022-04-10 06:57:47 -0400

    hist

---
+++ 5.0/_latest_commit_tag.sh
@@ -0,0 +1,1 @@
+export latestcontainertag=jgwill/zeus:giamongodb-2204100604
--- 5.0/run-docker.sh
+++ 5.0/run-docker.sh
@@ -2,6 +2,7 @@
 cmd=bash
 if [ "$dockermode" == "bg" ];then cmd="";fi
 
+#jgwill/zeus:giamongodb-2204100604
 echo "docker run $dockermodeargs --name $containername \
 	-v $(pwd):/work \
 	-p 27017:27017 \
